178e2e8f902d0af325fe246a176daed0e696c53c
diwuxiaoxu <38445712+diwuxiaoxu@users.noreply.github.com>
2020-06-23 21:30:14 +0800

    Add files via upload

--- XGBoost/XGBoost_test.py
+++ XGBoost/XGBoost_test.py
@@ -4,7 +4,22 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
+"""
+XGBClassifier(max_depth=3,
+learning_rate=0.1, 
+n_estimators=100, 
+silent=True, 
+objective='binary:logistic', 
+booster='gbtree', n_jobs=1, 
+nthread=None, gamma=0, 
+min_child_weight=1, max_delta_step=0, 
+subsample=1, colsample_bytree=1,
+colsample_bylevel=1, reg_alpha=0,
+reg_lambda=1, scale_pos_weight=1,
+base_score=0.5, random_state=0,
+seed=None, missing=None, **kwargs)
 
+"""
 if __name__ == '__main__':
     data = pd.read_csv('../data/train_binary.csv', header=0)
 
